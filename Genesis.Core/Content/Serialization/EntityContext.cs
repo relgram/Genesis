f00086d699a3d2fa7f994203ef2b23f6125b4ecf
Ryan Elgram <ryan.elgram@gmail.com>
2024-12-04 17:00:11 -0800

    Create EntityContext.cs

---
+++ Genesis.Core/Content/Serialization/EntityContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+using Genesis.Core.Entities;
+
+namespace Genesis.Core.Content.Serialization;
+
+[JsonSerializable(typeof(Guid))]
+[JsonSerializable(typeof(Dynamic))]
+[JsonSerializable(typeof(string))]
+[JsonSerializable(typeof(Actor[]))]
+[JsonSerializable(typeof(Area[]))]
+[JsonSerializable(typeof(Effect[]))]
+[JsonSerializable(typeof(Item[]))]
+[JsonSerializable(typeof(Locker[]))]
+[JsonSerializable(typeof(Player[]))]
+[JsonSerializable(typeof(Portal[]))]
+[JsonSerializable(typeof(Room[]))]
+[JsonSerializable(typeof(Zone[]))]
+[JsonSourceGenerationOptions(WriteIndented = true)]
+public partial class EntityContext : JsonSerializerContext
+{
+}
